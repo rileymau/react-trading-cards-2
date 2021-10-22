478e267af3537720b080f15d2c43ff644e863f22
Molly Riley <mo.riley@gmail.com>
2021-10-22 20:14:13 +0000

    finished step 2

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -1,54 +1,57 @@
-const tradingCardData = [
-  {
-    name: 'Balloonicorn',
-    skill: 'video games',
-    imgUrl: '/static/img/balloonicorn.jpg',
-    cardId: 1,
-  },
-  {
-    name: 'Float',
-    skill: 'baking pretzels',
-    imgUrl: '/static/img/float.jpg',
-    cardId: 2,
-  },
-  {
-    name: 'Llambda',
-    skill: 'knitting scarves',
-    imgUrl: '/static/img/llambda.jpg',
-    cardId: 3,
-  },
-  {
-    name: 'Off-By-One',
-    skill: 'climbing mountains',
-    imgUrl: '/static/img/off-by-one.jpeg',
-    cardId: 4,
-  },
-  {
-    name: 'Seed.py',
-    skill: 'making curry dishes',
-    imgUrl: '/static/img/seedpy.jpeg',
-    cardId: 5,
-  },
-  {
-    name: 'Polymorphism',
-    skill: 'costumes',
-    imgUrl: '/static/img/polymorphism.jpeg',
-    cardId: 6,
-  },
-  {
-    name: 'Short Stack Overflow',
-    skill: 'ocean animal trivia',
-    imgUrl: '/static/img/shortstack-overflow.jpeg',
-    cardId: 7,
-  },
-  {
-    name: 'Merge',
-    skill: 'bullet journaling',
-    imgUrl: '/static/img/merge.png',
-    cardId: 8,
-  },
-];
 
+//commented out on step 2, pulling data from server.py @app.route("/cards.json") instead. 
+// const tradingCardData = [
+//   {
+//     name: 'Balloonicorn',
+//     skill: 'video games',
+//     imgUrl: '/static/img/balloonicorn.jpg',
+//     cardId: 1,
+//   },
+//   {
+//     name: 'Float',
+//     skill: 'baking pretzels',
+//     imgUrl: '/static/img/float.jpg',
+//     cardId: 2,
+//   },
+//   {
+//     name: 'Llambda',
+//     skill: 'knitting scarves',
+//     imgUrl: '/static/img/llambda.jpg',
+//     cardId: 3,
+//   },
+//   {
+    // name: 'Off-By-One',
+    // skill: 'climbing mountains',
+  //   // imgUrl: '/static/img/off-by-one.jpeg',
+  //   cardId: 4,
+  // },
+  // {
+  //   name: 'Seed.py',
+  //   skill: 'making curry dishes',
+  //   imgUrl: '/static/img/seedpy.jpeg',
+  //   cardId: 5,
+  // },
+  // {
+  //   name: 'Polymorphism',
+  //   skill: 'costumes',
+  //   imgUrl: '/static/img/polymorphism.jpeg',
+//   //   cardId: 6,
+//   },
+//   {
+//     name: 'Short Stack Overflow',
+//     skill: 'ocean animal trivia',
+//     imgUrl: '/static/img/shortstack-overflow.jpeg',
+//     cardId: 7,
+//   },
+//   {
+//     name: 'Merge',
+//     skill: 'bullet journaling',
+//     imgUrl: '/static/img/merge.png',
+//     cardId: 8,
+//   },
+// ];
+
+//make a div for each trading card and its props
 function TradingCard(props) {
   return (
     <div className="card">
@@ -61,16 +64,24 @@ function TradingCard(props) {
 
 function TradingCardContainer() {
 
-  // make placeholder
-  const floatCard = {
-    name: 'Float',
-    skill: 'baking pretzels',
-    imgUrl: '/static/img/float.jpg'
-  };
-
+  // make placeholder. commented out on step 2. 
+  // const floatCard = {
+  //   name: 'Float',
+  //   skill: 'baking pretzels',
+  //   imgUrl: '/static/img/float.jpg'
+  // };
+  
   //useState hook with initial float card
-  const [cards, setCards] = React.useState([floatCard])
+  const [cards, setCards] = React.useState([])
 
+  //added with step 2 to useState in loop. 
+  //jsonify on servery.py, receive as .json on jsx page.  
+  // response linee 82 feeds in to date line 83.  
+  React.useEffect(() => {
+    fetch('/cards.json')
+    .then((response) => response.json())
+    .then((data) => setCards(data.cards))
+  }, [])
 
   //loop card in card date from server.py, append to array of components
   const tradingCards = [];
